3054c58f2b77863868ab8f832ea5ba146544573c
Dithmi2004 <dithmiprameesha@gmail.com>
2026-07-07 19:28:29 +0530

    Implement manager dashboard APIs

---
+++ backend/src/controllers/dashboardController.js
@@ -0,0 +1,36 @@
+const dashboardService = require("../services/dashboardService");
+const asyncHandler = require("../utils/asyncHandler");
+const { successResponse } = require("../utils/apiResponse");
+
+const getSummary = asyncHandler(async (req, res) => {
+  const data = await dashboardService.getSummary();
+  return successResponse(res, "Dashboard summary fetched successfully", data);
+});
+
+const getSubmissionStatus = asyncHandler(async (req, res) => {
+  const data = await dashboardService.getSubmissionStatus();
+  return successResponse(res, "Submission status fetched successfully", data);
+});
+
+const getProjectDistribution = asyncHandler(async (req, res) => {
+  const data = await dashboardService.getProjectDistribution();
+  return successResponse(res, "Project distribution fetched successfully", data);
+});
+
+const getTasksTrend = asyncHandler(async (req, res) => {
+  const data = await dashboardService.getTasksTrend();
+  return successResponse(res, "Tasks trend fetched successfully", data);
+});
+
+const getRecentActivity = asyncHandler(async (req, res) => {
+  const data = await dashboardService.getRecentActivity();
+  return successResponse(res, "Recent activity fetched successfully", data);
+});
+
+module.exports = {
+  getSummary,
+  getSubmissionStatus,
+  getProjectDistribution,
+  getTasksTrend,
+  getRecentActivity,
+};
